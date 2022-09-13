7d151f63d5cfdcda0b616b39d92e44fe27dcbe07
Your Name <you@example.com>
2022-09-13 12:20:57 +0530

    new files

---
+++ src/main/java/com/demo/constant/ApplicationConstant.java
@@ -0,0 +1,8 @@
+package com.demo.constant;
+
+public class ApplicationConstant {
+
+	public static final String home = "/home";
+	public static final String employee = "/employee";
+	public static final String id = "/{id}";
+}
--- src/main/java/com/demo/controller/EmployeeController.java
+++ src/main/java/com/demo/controller/EmployeeController.java
@@ -2,8 +2,6 @@ package com.demo.controller;
 
 import java.util.List;
 
-import javax.websocket.OnError;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.constant.ApplicationConstant;
 import com.demo.entity.Employee;
 import com.demo.service.EmployeeService;
 
@@ -22,32 +21,32 @@ public class EmployeeController{
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@GetMapping("/home")
+	@GetMapping(ApplicationConstant.home)
 	public String home() {
 		return employeeService.home();
 	}
 	
-	@GetMapping("/employee")
+	@GetMapping(ApplicationConstant.employee)
 	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
 	}
 	
-	@GetMapping("/employee/{id}")
+	@GetMapping(ApplicationConstant.employee + ApplicationConstant.id)
 	public Employee getEmployee(@PathVariable String id) {
 		return employeeService.getEmployee(id);
 	}
 	
-	@PostMapping("/employee")
+	@PostMapping(ApplicationConstant.employee)
 	public Employee addEmployee(@RequestBody Employee employee) {
 		return employeeService.addEmployee(employee);
 	}
 	
-	@PutMapping("/employee")
+	@PutMapping(ApplicationConstant.employee)
 	public Employee updateEmployee(@RequestBody Employee employee) {
 		return employeeService.updateEmployee(employee);
 	}
 	
-	@DeleteMapping("/employee/{id}")
+	@DeleteMapping(ApplicationConstant.employee + ApplicationConstant.id)
 	public String deleteEmployee(@PathVariable String id) {
 		return employeeService.deleteEmployee(id);
 	}
